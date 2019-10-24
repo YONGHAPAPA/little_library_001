8e01a474fdd41fc32b91034a73bbbd2cd402a47b
YONGHAPAPA <zerobreaker@gmail.com>
2019-10-24 18:06:38 +0900

    20191024end

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,14 +1,21 @@
 var Book = require('../models/book');
+var BookInstance = require('../models/bookinstance');
 var async = require('async');
 
 exports.index = function(req, res){
-    //res.send("Book Index...");
+    
+    //샘플로 텍스트 찍어보기 
+    //res.send("Book Index..."); 
 
+    //각 Collection 별로 Count 
     async.parallel({
         book_cnt : function(cb){
             Book.countDocuments({},cb);
+        }, 
+        book_instance_cnt : function(cb){
+            BookInstance.countDocuments({}, cb);
         }
     }, function(err, results){
-        res.render('index', {title:'', error:err, data:results})
+        res.render('index', {title:'little library', error:err, data:results})
     });
 }
