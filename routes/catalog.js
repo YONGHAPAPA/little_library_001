83d851204c7648f8706fa760fe396dab458b2ded
YONGHAPAPA <zerobreaker@gmail.com>
2019-10-24 11:45:00 +0900

    dev

---
+++ routes/catalog.js
@@ -0,0 +1,10 @@
+var express = require('Express');
+var router = express.Router();
+
+//Event Handler Define (S)
+//var book_controller = require('../controllers/bookController');
+//Event Handler Define (E)
+
+//router.get('/', book_controller.index);
+
+module.exports = router;  
